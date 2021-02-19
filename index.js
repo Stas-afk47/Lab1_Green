af477fc6a559034142e77738bb753b4f7a4361ae
Станислав Дмитриевич Кирчанов <19300517@live.inueco.ru>
2021-02-19 10:42:10 +0500

    4

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 function bank() {
-  let x,percent,length
-  x = prompt ();
+  let summ,percent,length,monthchanges,balancechange,percentchange
+  summ = prompt ();
   percent= prompt ();
   length = prompt ();
+  monthchanges = prompt ();
   if (0>percent>100) {
-    return 'Данные некорректны'
+    return console.log('Данные некорректны')
   }
-  if (Number.isInteger (x)==false) {
-    return 'Данные некорректны'
+  if (Number.isInteger (summ)==false) {
+    return console.log('Данные некорректны')
   }
   if (Number.isInteger (length)==false) {
-    return 'Данные некорректны'
+    return console.log('Данные некорректны')
+  }
+  percent=percent/100;
+  console.log('Начальная сумма = '+summ);
+  console.log('Начальный процент = '+percent);
+  
+  for (let y=0; y<length; y++){
+    balancechange=summ;
+    balancechange = balancechange + balancechange*percent;
+    balancechange = balancechange+monthchanges;
+    percentchange = 1-summ/balancechange;
+    console.log(balancechange);
+    summ=balancechange;
   }
 }
